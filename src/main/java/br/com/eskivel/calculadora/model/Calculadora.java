2dd7609a5f0c19289d6d3c67a0c32b9296dd1640
Heber Silva <heber.as@gmail.com>
2019-08-15 19:57:35 -0300

    f5

---
+++ src/main/java/br/com/eskivel/calculadora/model/Calculadora.java
@@ -0,0 +1,79 @@
+package br.com.eskivel.calculadora.model;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Calculadora {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private double n1;
+	private double n2;
+	private double resultado;
+	
+	@Enumerated(EnumType.STRING) 
+	private OperadorCalc operador;
+
+	
+	public double soma() {
+		return n1 + n2;
+	}
+
+	public double subtracao() {
+		return n1 - n2;
+	}
+	
+	public double multiplicacao() {
+		return n1 * n2;
+	}
+	
+	public double divisao() {
+		return n1 / n2;
+	}
+	
+	public double getN2() {
+		return n2;
+	}
+
+	public void setN1(double n1) {
+		this.n1 = n1;
+	}
+
+	public void setN2(double n2) {
+		this.n2 = n2;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public OperadorCalc getOperador() {
+		return operador;
+	}
+
+	public double getN1() {
+		return n1;
+	}
+
+	public void setOperador(OperadorCalc operador) {
+		this.operador = operador;
+	}
+
+	public double getResultado() {
+		return resultado;
+	}
+
+	public void setResultado(double resultado) {
+		this.resultado = resultado;
+	}
+	
+
+}
---
+++ src/main/java/br/com/eskivel/calculadora/model/OperadorCalc.java
@@ -0,0 +1,55 @@
+package br.com.eskivel.calculadora.model;
+
+public enum OperadorCalc {
+
+	MULTIPLICACAO, DIVISAO, SOMA, SUBTRACAO;
+
+}
+
+//	private Long id;
+//	private String multiplicacao;
+//	private String divisao;
+//	private String soma;
+//	private String subtracao;
+
+//	public OperadorCalc(String soma) {
+//		
+//		this.soma = soma;
+//	}
+//
+//	public Long getId() {
+//		return id;
+//	}
+//	public void setId(Long id) {
+//		this.id = id;
+//	}
+//
+//	public String getMultiplicacao() {
+//		return multiplicacao;
+//	}
+//	public void setMultiplicacao(String multiplicacao) {
+//		this.multiplicacao = multiplicacao;
+//	}
+//	public String getDivisao() {
+//		return divisao;
+//	}
+//	public void setDivisao(String divisao) {
+//		this.divisao = divisao;
+//	}
+//	public String getSoma() {
+//		return soma;
+//	}
+//	public void setSoma(String soma) {
+//		this.soma = soma;
+//	}
+//	public String getSubtracao() {
+//		return subtracao;
+//	}
+//	public void setSubtracao(String subtracao) {
+//		this.subtracao = subtracao;
+//	}
+//	
+//	
+//}
+//	
+//	
