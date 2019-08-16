e558eb776b4f985b45446327f01fe558a0b00a78
Heber Silva <heber.as@gmail.com>
2019-08-16 11:44:43 -0300

    f5

--- src/main/java/br/com/eskivel/calculadora/repository/CalculadoraRepository.java
+++ src/main/java/br/com/eskivel/calculadora/repository/CalculadoraRepository.java
@@ -5,7 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.eskivel.calculadora.model.Calculadora;
 
 public interface CalculadoraRepository extends JpaRepository<Calculadora, Long> {
-//
-//	Long findBycalculando(long n1, long n2, OperadorCalc operador);
+
 }
-// 
