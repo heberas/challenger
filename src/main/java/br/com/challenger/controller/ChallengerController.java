0a4675073800dda3ed6e3efc76169593c4c108b5
Heber Silva <heber.as@gmail.com>
2023-02-04 15:55:36 -0300

    adding new java & spring version

--- src/main/java/br/com/challenger/controller/ChallengerController.java
+++ src/main/java/br/com/challenger/controller/ChallengerController.java
@@ -1,8 +1,7 @@
 package br.com.challenger.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-
+import br.com.challenger.model.Password;
+import br.com.challenger.service.ValidatePasswordService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,24 +10,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.challenger.model.Password;
-import br.com.challenger.service.ValidatePasswordService;
-
 @RestController
 @RequestMapping("/validate/password")
 public class ChallengerController {
 
-	private final Logger logger = LoggerFactory.getLogger(ChallengerController.class);
+    private final Logger logger = LoggerFactory.getLogger(ChallengerController.class);
 
-	@Autowired
-	public ValidatePasswordService validate;
+    @Autowired
+    public ValidatePasswordService validate;
 
-	@PostMapping
-	public boolean validatePassword(@RequestBody Password password,
-			HttpServletRequest request) {
-		logger.info("m=validatePassword", password, request);
-
-		return validate.isValid(password);
-
-	}
+    @PostMapping
+    public boolean validatePassword(@RequestBody Password password) {
+        logger.info("m=validatePassword", password);
+        return validate.isValid(password);
+    }
 }
